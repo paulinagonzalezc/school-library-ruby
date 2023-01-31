17f956deec52af2da779a39334cc5ac4c701482c
Paulina Gonzalez <paulinagzzc@gmail.com>
2023-01-31 13:08:21 -0600

    Add linters and first class

---
+++ person.rb
@@ -0,0 +1,19 @@
+class Person
+  def initialize(age, name = "Unknown", parent_permission = true)
+    @id = Random.rand(1..100)
+    @name = name
+    @age = age
+    @parent_permission = parent_permission
+  end
+  #accesor get and set method
+  attr_accessor :name, :age
+  #accesor get method
+  attr_reader :id
+
+  def can_use_services?
+     is_of_age? || @parent_permission
+  end
+
+  def is_of_age?
+    @age >= 18
+  end
