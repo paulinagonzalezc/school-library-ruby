22a0df413903a0ef1ad63cde40ea696fac9edcdf
Paulina Gonzalez <paulinagzzc@gmail.com>
2023-01-31 13:23:44 -0600

    Add student and teacher classes

--- person.rb
+++ person.rb
@@ -1,19 +1,20 @@
 class Person
-  def initialize(age, name = "Unknown", parent_permission = true)
+  def initialize(age, name = 'Unknown', parent_permission: true)
     @id = Random.rand(1..100)
     @name = name
     @age = age
     @parent_permission = parent_permission
   end
-  #accesor get and set method
+  # accesor get and set method
   attr_accessor :name, :age
-  #accesor get method
+  # accesor get method
   attr_reader :id
 
   def can_use_services?
-     is_of_age? || @parent_permission
+    is_of_age? || @parent_permission
   end
 
-  def is_of_age?
+  def of_age?
     @age >= 18
   end
+end
