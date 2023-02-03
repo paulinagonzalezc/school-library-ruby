6b6e400cdc23a4ae9cef0c61d8128f3d67347eb0
Paulina Gonzalez <paulinagzzc@gmail.com>
2023-02-02 23:04:04 -0600

    Set up associations

--- person.rb
+++ person.rb
@@ -1,18 +1,19 @@
 require './decorator'
 
 class Person < Nameable
+  # accesor get and set method
+  attr_accessor :name, :age
+  # accesor get method
+  attr_reader :id, :rentals
+
   def initialize(age, name = 'Unknown', parent_permission: true)
     @id = Random.rand(1..100)
     @name = name
     @age = age
     @parent_permission = parent_permission
+    @rentals = []
     super()
   end
-
-  # accesor get and set method
-  attr_accessor :name, :age
-  # accesor get method
-  attr_reader :id
 
   def can_use_services?
     of_age? || @parent_permission
--- student.rb
+++ student.rb
@@ -1,12 +1,18 @@
 require './person'
+require './classroom'
 
 class Student < Person
-  def initialize(age, classroom, name = 'Unknown', parent_permission: true)
+
+  def initialize(age, name = 'Unknown', parent_permission: true)
     super(age, name, parent_permission)
-    @classroom = classroom
   end
 
   def play_hooky
     '¯\(ツ)/¯'
+  end
+
+  def classroom(classroom)
+    @classroom = classroom
+    classroom.students.push(self) unless classroom.students.include?(self)
   end
 end
