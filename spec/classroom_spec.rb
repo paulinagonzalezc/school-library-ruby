6c2425f1aed527a5661e26722b7f95934b2ceced
Paulina Gonzalez <paulinagzzc@gmail.com>
2023-02-08 16:29:38 -0600

    Fix linter errors

--- spec/classroom_spec.rb
+++ spec/classroom_spec.rb
@@ -1,18 +1,18 @@
-require_relative 'classroom'
-require_relative 'student'
+require_relative '../classroom'
+require_relative '../student'
 
 describe Classroom do
-  context ‘When testing the Classroom class’ do
-    label = ‘Math’
-    student = Student.new(20, ‘John’, parent_permission: true)
+  context 'When testing the Classroom class' do
+    label = 'Math'
+    student = Student.new(20, 'John', parent_permission: true)
     classroom = Classroom.new(label)
     classroom.add_student(student)
 
-    it ‘Validate the label of the classroom’ do
+    it 'Validate the label of the classroom' do
       expect(classroom.label).to eq label
     end
 
-    it ‘Validate the student of the classroom’ do
+    it 'Validate the student of the classroom' do
       expect(classroom.students[0]).to eq student
     end
   end
